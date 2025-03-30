9ce216a028a1c9f1674e3b898115ea892757f468
Rayyan-Gular <148619861+Rayyan-Gular@users.noreply.github.com>
2025-03-30 05:48:39 +0500

    Update theLongMessageThngy.py

--- theLongMessageThngy.py
+++ theLongMessageThngy.py
@@ -1,12 +1,12 @@
 import pyperclip, pyautogui, time
 
 time.sleep(5)
-pyperclip.copy("sorry" * 200)
+pyperclip.copy("meow" * 200)
 
 for i in range(71):
     pyautogui.hotkey('ctrl', 'v')
     pyautogui.press('enter')
     time.sleep(1)
 
-pyautogui.typewrite("sorry" * 43)
+pyautogui.typewrite("meow" * 43)
 pyautogui.press('enter')
